e4af28cebc754c19753d11de681bdea8ae2c0abf
litluo <litluo@mail.ustc.edu.cn>
2025-05-25 16:01:08 +0800

    WIP on main

---
+++ Avatar.js
@@ -0,0 +1,446 @@
+avatar_list = [
+    {
+        "_id": 1001,
+        "Name": "三月七",
+        "Element": "Ice",
+        "Path": "Preservation"
+    },
+    {
+        "_id": 1002,
+        "Name": "丹恒",
+        "Element": "Wind",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1003,
+        "Name": "姬子",
+        "Element": "Fire",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1004,
+        "Name": "瓦尔特",
+        "Element": "Imaginary",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1005,
+        "Name": "卡芙卡",
+        "Element": "Elec",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1006,
+        "Name": "银狼",
+        "Element": "Quantum",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1008,
+        "Name": "阿兰",
+        "Element": "Elec",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1009,
+        "Name": "艾丝妲",
+        "Element": "Fire",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1013,
+        "Name": "黑塔",
+        "Element": "Ice",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1014,
+        "Name": "Saber",
+        "Element": "Wind",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1015,
+        "Name": "Archer",
+        "Element": "Quantum",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1101,
+        "Name": "布洛妮娅",
+        "Element": "Wind",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1102,
+        "Name": "希儿",
+        "Element": "Quantum",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1103,
+        "Name": "希露瓦",
+        "Element": "Elec",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1104,
+        "Name": "杰帕德",
+        "Element": "Ice",
+        "Path": "Preservation"
+    },
+    {
+        "_id": 1105,
+        "Name": "娜塔莎",
+        "Element": "Phys",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1106,
+        "Name": "佩拉",
+        "Element": "Ice",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1107,
+        "Name": "克拉拉",
+        "Element": "Phys",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1108,
+        "Name": "桑博",
+        "Element": "Wind",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1109,
+        "Name": "虎克",
+        "Element": "Fire",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1110,
+        "Name": "玲可",
+        "Element": "Quantum",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1111,
+        "Name": "卢卡",
+        "Element": "Phys",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1112,
+        "Name": "托帕&账账",
+        "Element": "Fire",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1201,
+        "Name": "青雀",
+        "Element": "Quantum",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1202,
+        "Name": "停云",
+        "Element": "Elec",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1203,
+        "Name": "罗刹",
+        "Element": "Imaginary",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1204,
+        "Name": "景元",
+        "Element": "Elec",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1205,
+        "Name": "刃",
+        "Element": "Wind",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1206,
+        "Name": "素裳",
+        "Element": "Phys",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1207,
+        "Name": "驭空",
+        "Element": "Imaginary",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1208,
+        "Name": "符玄",
+        "Element": "Quantum",
+        "Path": "Preservation"
+    },
+    {
+        "_id": 1209,
+        "Name": "彦卿",
+        "Element": "Ice",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1210,
+        "Name": "桂乃芬",
+        "Element": "Fire",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1211,
+        "Name": "白露",
+        "Element": "Elec",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1212,
+        "Name": "镜流",
+        "Element": "Ice",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1213,
+        "Name": "丹恒 · 饮月",
+        "Element": "Imaginary",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1214,
+        "Name": "雪衣",
+        "Element": "Quantum",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1215,
+        "Name": "寒鸦",
+        "Element": "Phys",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1217,
+        "Name": "藿藿",
+        "Element": "Wind",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1218,
+        "Name": "椒丘",
+        "Element": "Fire",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1220,
+        "Name": "飞霄",
+        "Element": "Wind",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1221,
+        "Name": "云璃",
+        "Element": "Phys",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1222,
+        "Name": "灵砂",
+        "Element": "Fire",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1223,
+        "Name": "貊泽",
+        "Element": "Elec",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1224,
+        "Name": "三月七",
+        "Element": "Imaginary",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1225,
+        "Name": "忘归人",
+        "Element": "Fire",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1301,
+        "Name": "加拉赫",
+        "Element": "Fire",
+        "Path": "Abundance"
+    },
+    {
+        "_id": 1302,
+        "Name": "银枝",
+        "Element": "Phys",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1303,
+        "Name": "阮 · 梅",
+        "Element": "Ice",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1304,
+        "Name": "砂金",
+        "Element": "Imaginary",
+        "Path": "Preservation"
+    },
+    {
+        "_id": 1305,
+        "Name": "真理医生",
+        "Element": "Imaginary",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1306,
+        "Name": "花火",
+        "Element": "Quantum",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1307,
+        "Name": "黑天鹅",
+        "Element": "Wind",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1308,
+        "Name": "黄泉",
+        "Element": "Elec",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1309,
+        "Name": "知更鸟",
+        "Element": "Phys",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1310,
+        "Name": "流萤",
+        "Element": "Fire",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1312,
+        "Name": "米沙",
+        "Element": "Ice",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1313,
+        "Name": "星期日",
+        "Element": "Imaginary",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1314,
+        "Name": "翡翠",
+        "Element": "Quantum",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1315,
+        "Name": "波提欧",
+        "Element": "Phys",
+        "Path": "Hunt"
+    },
+    {
+        "_id": 1317,
+        "Name": "乱破",
+        "Element": "Imaginary",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1401,
+        "Name": "大黑塔",
+        "Element": "Ice",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1402,
+        "Name": "阿格莱雅",
+        "Element": "Elec",
+        "Path": "Remembrance"
+    },
+    {
+        "_id": 1403,
+        "Name": "缇宝",
+        "Element": "Quantum",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 1404,
+        "Name": "万敌",
+        "Element": "Imaginary",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1405,
+        "Name": "那刻夏",
+        "Element": "Wind",
+        "Path": "Erudition"
+    },
+    {
+        "_id": 1406,
+        "Name": "赛飞儿",
+        "Element": "Quantum",
+        "Path": "Nihility"
+    },
+    {
+        "_id": 1407,
+        "Name": "遐蝶",
+        "Element": "Quantum",
+        "Path": "Remembrance"
+    },
+    {
+        "_id": 1408,
+        "Name": "白厄",
+        "Element": "Phys",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 1409,
+        "Name": "风堇",
+        "Element": "Wind",
+        "Path": "Remembrance"
+    },
+    {
+        "_id": 8002,
+        "Name": "开拓者",
+        "Element": "Phys",
+        "Path": "Destruction"
+    },
+    {
+        "_id": 8004,
+        "Name": "开拓者",
+        "Element": "Fire",
+        "Path": "Preservation"
+    },
+    {
+        "_id": 8006,
+        "Name": "开拓者",
+        "Element": "Imaginary",
+        "Path": "Harmony"
+    },
+    {
+        "_id": 8008,
+        "Name": "开拓者",
+        "Element": "Ice",
+        "Path": "Remembrance"
+    }
+]
